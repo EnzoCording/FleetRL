94d8611b2cfbbb57a4514eb9847ba41f1ca77cfc
Enzo Cording <enzocording@web.de>
2023-04-16 15:10:02 +0200

    Removed super

--- src-FleetRL/FleetRL/utils/battery_depreciation/my_battery_depreciation.py
+++ src-FleetRL/FleetRL/utils/battery_depreciation/my_battery_depreciation.py
@@ -4,5 +4,4 @@ from FleetRL.utils.battery_depreciation.battery_dep_base import BatteryDepreciat
 class MyBatteryDepreciation(BatteryDepreciationBase):
 
     def calculate(self, charging_time: float, initial_soc, temperature) -> float:
-        x = super().calculate(charging_time, initial_soc, temperature)
-        return x * 0.1
+        return 0.1
