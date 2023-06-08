b929c5c9f911ebe6fde2ce52864eff41452b6f3d
Enzo Cording <enzocording@web.de>
2023-06-08 22:08:52 +0200

    Changed setup py and req location

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='FleetRL', version='0.0.1')
--- src-FleetRL/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name='FleetRL',
-      version='0.0.1',
-      install_requires=['gym==0.26.2']  # And any other dependencies required
-      )
