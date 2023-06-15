d7633c5881bbf5ac445fd9f74d20ce9b579972ed
Enzo Cording <enzocording@web.de>
2023-06-15 21:49:15 +0200

    included new gcp setup file and changed penalties

--- src/FleetRL/fleet_env/config/score_config.py
+++ src/FleetRL/fleet_env/config/score_config.py
@@ -8,8 +8,8 @@ class ScoreConfig:
         # TODO give overcharging/underloading penalty not only when the car departs, but
         # also in realtime when the battery dips below or goes above the healthy range
         # And maybe increase the penalty the farther the agent gets away from these boundaries
-        self.penalty_invalid_action = -500
-        self.penalty_overcharging = -100
+        self.penalty_invalid_action = -0.15
+        self.penalty_overcharging = -0.05
 
         # possible reward: money spent/earned due to buying/selling electricity for charging/discharging
         # but these rewards fluctuate at runtime and so are not configured here explicitly
