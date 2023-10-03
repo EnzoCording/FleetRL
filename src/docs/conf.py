0b387fe8a8d3e892b2d7b84f018225955f3df138
Enzo Cording <enzocording@web.de>
2023-10-03 20:17:11 +0200

    Update conf.py

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -11,41 +11,41 @@ import os
 import sys
 from typing import Dict
 
-import FleetRL
-from FleetRL.fleet_env.config.ev_config import EvConfig
-from FleetRL.fleet_env.config.score_config import ScoreConfig
-from FleetRL.fleet_env.config.time_config import TimeConfig
+# import FleetRL
+# from FleetRL.fleet_env.config.ev_config import EvConfig
+# from FleetRL.fleet_env.config.score_config import ScoreConfig
+# from FleetRL.fleet_env.config.time_config import TimeConfig
 
-from FleetRL.fleet_env.episode import Episode
+# from FleetRL.fleet_env.episode import Episode
 
-from FleetRL.utils.data_processing.data_processing import DataLoader
-from FleetRL.utils.ev_charging.ev_charger import EvCharger
-from FleetRL.utils.load_calculation.load_calculation import LoadCalculation, CompanyType
+# from FleetRL.utils.data_processing.data_processing import DataLoader
+# from FleetRL.utils.ev_charging.ev_charger import EvCharger
+# from FleetRL.utils.load_calculation.load_calculation import LoadCalculation, CompanyType
 
-from FleetRL.utils.normalization.normalization import Normalization
-from FleetRL.utils.normalization.oracle_normalization import OracleNormalization
-from FleetRL.utils.normalization.unit_normalization import UnitNormalization
+# from FleetRL.utils.normalization.normalization import Normalization
+# from FleetRL.utils.normalization.oracle_normalization import OracleNormalization
+# from FleetRL.utils.normalization.unit_normalization import UnitNormalization
 
-from FleetRL.utils.observation.observer_with_building_load import ObserverWithBuildingLoad
-from FleetRL.utils.observation.observer_price_only import ObserverPriceOnly
-from FleetRL.utils.observation.observer import Observer
-from FleetRL.utils.observation.observer_with_pv import ObserverWithPV
-from FleetRL.utils.observation.observer_bl_pv import ObserverWithBoth
-from FleetRL.utils.observation.observer_soc_time_only import ObserverSocTimeOnly
+# from FleetRL.utils.observation.observer_with_building_load import ObserverWithBuildingLoad
+# from FleetRL.utils.observation.observer_price_only import ObserverPriceOnly
+# from FleetRL.utils.observation.observer import Observer
+# from FleetRL.utils.observation.observer_with_pv import ObserverWithPV
+# from FleetRL.utils.observation.observer_bl_pv import ObserverWithBoth
+# from FleetRL.utils.observation.observer_soc_time_only import ObserverSocTimeOnly
 
-from FleetRL.utils.time_picker.random_time_picker import RandomTimePicker
-from FleetRL.utils.time_picker.static_time_picker import StaticTimePicker
-from FleetRL.utils.time_picker.eval_time_picker import EvalTimePicker
-from FleetRL.utils.time_picker.time_picker import TimePicker
+# from FleetRL.utils.time_picker.random_time_picker import RandomTimePicker
+# from FleetRL.utils.time_picker.static_time_picker import StaticTimePicker
+# from FleetRL.utils.time_picker.eval_time_picker import EvalTimePicker
+# from FleetRL.utils.time_picker.time_picker import TimePicker
 
-from FleetRL.utils.battery_degradation.batt_deg import BatteryDegradation
-from FleetRL.utils.battery_degradation.empirical_degradation import EmpiricalDegradation
-from FleetRL.utils.battery_degradation.rainflow_sei_degradation import RainflowSeiDegradation
-from FleetRL.utils.battery_degradation.log_data_deg import LogDataDeg
+# from FleetRL.utils.battery_degradation.batt_deg import BatteryDegradation
+# from FleetRL.utils.battery_degradation.empirical_degradation import EmpiricalDegradation
+# from FleetRL.utils.battery_degradation.rainflow_sei_degradation import RainflowSeiDegradation
+# from FleetRL.utils.battery_degradation.log_data_deg import LogDataDeg
 
-from FleetRL.utils.data_logger.data_logger import DataLogger
+# from FleetRL.utils.data_logger.data_logger import DataLogger
 
-from FleetRL.utils.schedule.schedule_generator import ScheduleGenerator, ScheduleType
+# from FleetRL.utils.schedule.schedule_generator import ScheduleGenerator, ScheduleType
 
 # We CANNOT enable 'sphinxcontrib.spelling' because ReadTheDocs.org does not support
 # PyEnchant.
