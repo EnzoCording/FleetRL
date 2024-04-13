64d9a800ec7799c59b89b5e5a0c430fe79c236d1
Enzo Cording <enzocording@web.de>
2024-04-13 17:51:14 +0200

    Added detailed plot function

--- fleetrl/agent_eval/basic_evaluation.py
+++ fleetrl/agent_eval/basic_evaluation.py
@@ -12,6 +12,13 @@ import matplotlib.pyplot as plt
 import datetime as dt
 import matplotlib.dates as mdates
 
+import seaborn as sns
+import matplotlib
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+matplotlib.rcParams.update({'font.size': 16})
+
 from fleetrl.agent_eval.evaluation import Evaluation
 
 
@@ -211,3 +218,184 @@ class BasicEvaluation(Evaluation):
 
         plt.tight_layout()
         plt.show()
+
+    def plot_detailed_actions(self,
+                              start_date: str | pd.Timestamp,
+                              end_date: str | pd.Timestamp,
+                              rl_log: pd.DataFrame=None,
+                              uc_log: pd.DataFrame=None,
+                              dist_log: pd.DataFrame=None,
+                              night_log: pd.DataFrame=None):
+
+        assert any(df is not None for df in [rl_log, uc_log, dist_log, night_log]), "No log provided."
+
+        chosen_dfs = []
+        log_names = []
+
+        if rl_log is not None:
+            rl_log = rl_log[(rl_log["Time"] >= start_date) & (rl_log["Time"] <= end_date)]
+            df = pd.DataFrame({
+                'Date': rl_log["Time"],
+                'SOC': [rl_log["Observation"][i][0:4].mean() for i in range(len(rl_log))],
+                'Load': [rl_log["Observation"][i][29] for i in range(len(rl_log))],
+                'PV': [rl_log["Observation"][i][34] for i in range(len(rl_log))],
+                'Price': [rl_log["Observation"][i][10] / 10 for i in range(len(rl_log))],
+                'Action': [rl_log["Charging energy"][i][0:4].sum() * 4 for i in range(len(rl_log))],
+                'Free cap': [rl_log["Observation"][i][-8] / rl_log["Observation"][i][-9] for i in range(len(rl_log))],
+                'CF': rl_log["Cashflow"]
+            })
+            chosen_dfs.append(df)
+            log_names.append("RL-based charging")
+
+        if uc_log is not None:
+            uc_log = uc_log[(uc_log["Time"] >= start_date) & (uc_log["Time"] <= end_date)]
+            df_dumb = pd.DataFrame({
+                'Date': uc_log["Time"],
+                'SOC': [uc_log["Observation"][i][0:4].mean() for i in range(len(uc_log))],
+                'Load': [uc_log["Observation"][i][29] for i in range(len(uc_log))],
+                'PV': [uc_log["Observation"][i][34] for i in range(len(uc_log))],
+                'Price': [uc_log["Observation"][i][10] for i in range(len(uc_log))],
+                'Action': [uc_log["Charging energy"][i][0:4].sum() * 4 for i in range(len(uc_log))],
+                'Free cap': [uc_log["Observation"][i][-8] / uc_log["Observation"][i][-9] for i in range(len(uc_log))],
+                'CF': uc_log["Cashflow"]
+            })
+            chosen_dfs.append(df_dumb)
+            log_names.append("Uncontrolled charging")
+
+        if dist_log is not None:
+            dist_log = dist_log[(dist_log["Time"] >= start_date) & (dist_log["Time"] <= end_date)]
+            df_dist = pd.DataFrame({
+                'Date': dist_log["Time"],
+                'SOC': [dist_log["Observation"][i][0:4].mean() for i in range(len(dist_log))],
+                'Load': [dist_log["Observation"][i][29] for i in range(len(dist_log))],
+                'PV': [dist_log["Observation"][i][34] for i in range(len(dist_log))],
+                'Price': [dist_log["Observation"][i][10] for i in range(len(dist_log))],
+                'Action': [dist_log["Charging energy"][i][0:4].sum() * 4 for i in range(len(dist_log))],
+                'Free cap': [dist_log["Observation"][i][-8] / dist_log["Observation"][i][-9] for i in range(len(dist_log))],
+                'CF': dist_log["Cashflow"]
+            })
+            chosen_dfs.append(df_dist)
+            log_names.append("Distributed charging")
+
+        if night_log is not None:
+            night_log = night_log[(night_log["Time"] >= start_date) & (night_log["Time"] <= end_date)]
+            df_night = pd.DataFrame({
+                'Date': night_log["Time"],
+                'SOC': [night_log["Observation"][i][0:4].mean() for i in range(len(night_log))],
+                'Load': [night_log["Observation"][i][29] for i in range(len(night_log))],
+                'PV': [night_log["Observation"][i][34] for i in range(len(night_log))],
+                'Price': [night_log["Observation"][i][10] for i in range(len(night_log))],
+                'Action': [night_log["Charging energy"][i][0:4].sum() * 4 for i in range(len(night_log))],
+                'Free cap': [night_log["Observation"][i][-8] / night_log["Observation"][i][-9] for i in
+                             range(len(night_log))],
+                'CF': night_log["Cashflow"]
+            })
+            chosen_dfs.append(df_night)
+            log_names.append("Night charging")
+
+        # Create a subplot with 3 rows and 1 column, without sharing the x-axis
+        fig = make_subplots(rows=len(chosen_dfs)+1, cols=1, shared_xaxes=False, vertical_spacing=0.04,
+                            specs=[[{'secondary_y': True}] for _ in range(len(chosen_dfs)+1)],
+                            subplot_titles=(["Load, PV and Price",
+                                             *[f"{log_names[i]} - Money spent: €" + str(np.round(chosen_dfs[i]["CF"].sum() * -1, 1))
+                                             for i in range(len(chosen_dfs))]]),
+                            column_widths=[1200], row_heights=[270 for _ in range(len(chosen_dfs)+1)])
+
+        # Add traces for the first subplot
+        fig.add_trace(go.Scatter(x=chosen_dfs[0]["Date"], y=chosen_dfs[0]["Load"], name='Building Load', legendgroup="1"), row=1, col=1)
+        fig.add_trace(go.Scatter(x=chosen_dfs[0]["Date"], y=chosen_dfs[0]["PV"], name='PV', legendgroup="1"), row=1, col=1)
+        fig.add_trace(go.Scatter(x=chosen_dfs[0]["Date"], y=chosen_dfs[0]["Price"], name='Price', legendgroup="1"), row=1, col=1,
+                      secondary_y=True)
+
+        # # Add traces for the second subplot (you can change these as needed)
+        # fig.add_trace(go.Scatter(x=df["Date"], y=df_real["Action"], name='Charging power', legendgroup="2"), row=2, col=1)
+        # fig.add_trace(go.Scatter(x=df["Date"], y=df_real["SOC"], name='SOC', legendgroup="2"), row=2, col=1, secondary_y=True)
+
+        for i in range(len(chosen_dfs)):
+
+            # Add traces for the second subplot (you can change these as needed)
+            fig.add_trace(go.Scatter(x=chosen_dfs[i]["Date"],
+                                     y=chosen_dfs[i]["Action"],
+                                     name='Charging power',
+                                     legendgroup=f"{i+2}"),
+                          row=i+2, col=1)
+
+            fig.add_trace(go.Scatter(x=chosen_dfs[i]["Date"],
+                                     y=chosen_dfs[i]["SOC"],
+                                     name='SOC',
+                                     legendgroup=f"{i+2}"),
+                          row=i+2, col=1,secondary_y=True)
+
+        fig.update_xaxes(row=1, range=[start_date, end_date], matches="x")
+        fig.update_xaxes(row=2, range=[start_date, end_date], matches="x")
+
+        for i in range(len(chosen_dfs)+1):
+            fig.update_xaxes(row=i + 1, col=1, matches='x')
+
+        for i in range(1, len(chosen_dfs)+1):
+            fig.update_yaxes(
+                tickvals=[-20, 0, 20],  # Values at which ticks on this axis appear
+                ticktext=['-20', '0', '20'],  # Text that appears at the ticks
+                row=i + 1, col=1,  # Row and column of the subplot to update (adjust as needed)
+                secondary_y=False  # Set to True if updating the secondary y-axis
+            )
+
+        for i in range(1, len(chosen_dfs)+1):
+            fig.update_yaxes(
+                tickvals=[0, 0.8],  # Values at which ticks on this axis appear
+                ticktext=['0', '0.8'],  # Text that appears at the ticks
+                row=i + 1, col=1,  # Row and column of the subplot to update (adjust as needed)
+                secondary_y=True  # Set to True if updating the secondary y-axis
+            )
+
+        for i in range(1):
+            fig.update_yaxes(
+                tickvals=[-5, 0, 5, 10, 15],  # Values at which ticks on this axis appear
+                ticktext=['-5', '0', "5", "10", "15"],  # Text that appears at the ticks
+                row=i + 1, col=1,  # Row and column of the subplot to update (adjust as needed)
+                secondary_y=True  # Set to True if updating the secondary y-axis
+            )
+
+        fig.update_yaxes(row=1, col=1, range=[-6.5, 18], secondary_y=True)
+        for i in range(1, len(chosen_dfs)+1):
+            fig.update_yaxes(row=i+1, col=1, range=[-23, 23], secondary_y=False)
+
+        # Labels for primary y-axes
+        primary_labels = ["kW" for _ in range(len(chosen_dfs)+1)]
+
+        # Labels for secondary y-axes
+        secondary_labels = ["ct/kWh", *["SOC" for _ in range(len(chosen_dfs))]]
+
+        # Update primary y-axes labels
+        for i, label in enumerate(primary_labels):
+            fig.update_yaxes(title_text=label, row=i + 1, col=1, secondary_y=False)
+
+        # Update secondary y-axes labels
+        for i, label in enumerate(secondary_labels):
+            fig.update_yaxes(title_text=label, row=i + 1, col=1, secondary_y=True)
+
+        fig.update_layout(
+            width=1050,
+            height=1400,
+            margin=dict(l=35, r=45, t=25, b=25),
+            font=dict(size=16)
+        )
+
+        fig.update_layout(
+            legend_tracegroupgap=250,
+
+        )
+
+        # Update the x-axis to show the date without the year
+        fig.update_xaxes(
+            tickformat="%m-%d",  # Display only month and day
+        )
+
+        # Other plot updates
+        # ...
+        fig.update_xaxes(
+            tickformat="%m/%d %H:%M"  # Display abbreviated month name, day, hours, and minutes
+        )
+
+        # return the plot
+        return fig
