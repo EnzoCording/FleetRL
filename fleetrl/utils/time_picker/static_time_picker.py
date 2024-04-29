e269e5f377e3f21dd25f1c05ae1ff0f10fa78690
Enzo Cording <enzocording@web.de>
2024-04-29 21:26:39 +0200

    Add check to static time picker that year always matches

--- fleetrl/utils/time_picker/static_time_picker.py
+++ fleetrl/utils/time_picker/static_time_picker.py
@@ -16,6 +16,16 @@ class StaticTimePicker(TimePicker):
         self.start_time = start_time
 
     def choose_time(self, db: pd.Series, freq: str, end_cutoff: int) -> Timestamp:
+
+        first_year = db.iloc[0]["date"].year
+        last_year = db.iloc[-1]["date"].year
         chosen_start_time = pd.to_datetime(self.start_time)
+        chosen_year = chosen_start_time.year
+
+        # keep month, day and time but set the right year to match with schedule database
+        if (chosen_year < first_year) or (chosen_year > last_year):
+            print("Chosen year does not match db years. Adjusting to match start year in db...")
+            chosen_start_time = chosen_start_time + pd.DateOffset(years=first_year-chosen_year)
+
         # return start time
         return chosen_start_time
