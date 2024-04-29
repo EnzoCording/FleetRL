19bd644eb58a298062b4453dfe1ee991ca36b90a
Enzo Cording <enzocording@web.de>
2024-04-29 21:31:14 +0200

    Add methods to parent class

--- fleetrl/agent_eval/evaluation.py
+++ fleetrl/agent_eval/evaluation.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Evaluation:
 
     def evaluate_agent(self, env_kwargs: dict, norm_stats_path: str, model_path: str, seed: int = None):
@@ -17,5 +19,24 @@ class Evaluation:
         raise NotImplementedError("This is an abstract class")
 
     def plot_action_dist(self, rl_log, benchmark_log):
+
+        raise NotImplementedError("This is an abstract class")
+
+    def plot_detailed_actions(self,
+                              start_date: str | pd.Timestamp,
+                              end_date: str | pd.Timestamp,
+                              rl_log: pd.DataFrame=None,
+                              uc_log: pd.DataFrame=None,
+                              dist_log: pd.DataFrame=None,
+                              night_log: pd.DataFrame=None):
+
+        raise NotImplementedError("This is an abstract class")
+
+    @staticmethod
+    def _change_param(env_kwargs: dict, key: str, val) -> dict:
+
+        raise NotImplementedError("This is an abstract class")
+
+    def _get_from_obs(self, log: dict) -> pd.DataFrame:
 
         raise NotImplementedError("This is an abstract class")
